cbaebcd63e8c88aa132f1fc9223060c5f48a9ae9
Jack Moon <rokmc756@gmail.com>
2025-06-12 05:38:59 +0900

    Working Lustre with iSCSI

---
+++ check-backends-mounted.sh
@@ -0,0 +1,9 @@
+for i in `seq 201 208`
+do
+
+    ssh root@192.168.2.$i "mount -l | grep -E 'smb|ceph|iscsi|lustre'"
+
+done
+
+# for i in `find ./ -name "*2023-03-21*.csv"`; do grep --with-filename -E 'FATAL\:|ERROR\:' $i; done
+
--- check-ssh-conn.sh
+++ check-ssh-conn.sh
@@ -1,8 +1,8 @@
-for i in `seq 0 10`
+for i in `seq 1 10`
 do
 
     END_IP=$(( 200 + $i ))
-    echo $END_IP
+    # echo $END_IP
 
     nc -vz 192.168.2.$END_IP 22
     # ssh-keyscan 192.168.2.$END_IP
@@ -10,7 +10,21 @@ do
     ssh-keyscan 192.168.2.$END_IP >/dev/null 2>&1
 
 done
-echo 249
-nc -vz 192.168.2.249 22
-ssh-keyscan 192.168.2.249 >/dev/null 2>&1
+
+#echo 249
+#nc -vz 192.168.2.249 22
+#ssh-keyscan 192.168.2.249 >/dev/null 2>&1
+
+for i in `seq 8 9`
+do
+
+    END_IP=$(( 200 + $i ))
+    # echo $END_IP
+
+    nc -vz 192.168.2.$END_IP 22
+    # ssh-keyscan 192.168.2.$END_IP
+    ssh-keyscan 192.168.2.$END_IP >/dev/null 2>&1
+
+done
+
 
