6f36e3f41f128faf5535b5c62e47ca21d843db33
Jack Moon <rokmc756@gmail.com>
2025-06-12 19:26:06 +0900

    Working multiple mgs and filesysetm with dne

--- reboot-lustre.sh
+++ reboot-lustre.sh
@@ -1,15 +1,23 @@
 root_pass="changeme"
 
-for i in `seq 0 10`
+for i in `seq 1 10`
 do
 
     END_IP=$(( 200 + $i ))
     echo $END_IP
-
     sshpass -p "$root_pass" ssh -o StrictHostKeyChecking=no root@192.168.2.$END_IP "reboot"
 
 done
 
-echo 249
-sshpass -p "$root_pass" ssh -o StrictHostKeyChecking=no root@192.168.2.249 "reboot"
+# sshpass -p "$root_pass" ssh -o StrictHostKeyChecking=no root@192.168.2.249 "reboot"
+
+
+for j in `seq 48 49`
+do
+
+    END_IP=$(( 200 + $j ))
+    echo $END_IP
+    sshpass -p "$root_pass" ssh -o StrictHostKeyChecking=no root@192.168.2.$END_IP "reboot"
+
+done
 
